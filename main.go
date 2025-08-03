4440e3b2a50032423a7caf69f122915d06bdc6b4
Ikit24 <szszattila2@gmail.com>
2025-08-03 09:55:56 +0300

    implement REPL command interactions

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    usr_input := bufio.NewScanner(os.Stdin)
+
+    for {
+        fmt.Print("Pokedex > ")
+        usr_input.Scan()
+        text := usr_input.Text()
+        cleaned := strings.TrimSpace(text)
+        split_input := strings.Split(cleaned, " ")
+        if split_input[0] == "" {
+            fmt.Print("Error empty string, please enter a string.")
+            continue
+        }
+        fmt.Printf("Your command was: %s\n", split_input[0])
+    }
 }
