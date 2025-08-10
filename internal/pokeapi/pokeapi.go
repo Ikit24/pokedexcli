88ac71ad6c0a680c968b3a98770d6a31671a56ef
Ikit24 <szszattila2@gmail.com>
2025-08-10 10:00:32 +0300

    add struct for basic combat setup

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -35,3 +35,42 @@ type Pokemon struct {
         } `json:"type"`
     } `json:"types"`
 }
+
+type BattlePokemon struct {
+    Name           string `json:"name"`
+    ID             int    `json:"id"`
+    BaseExperience int    `json:"base_experience"`
+    Height         int    `json:"height"`
+    Weight         int    `json:"weight"`
+ 
+    Stats []struct {
+        BaseStat int `json:"base_stat"`
+        Stat     struct {
+            Name string `json:"name"`
+        } `json:"stat"`
+    } `json:"stats"`
+ 
+    Types []struct {
+        Type struct {
+            Name string `json:"name"`
+        } `json:"type"`
+    } `json:"types"`
+ 
+    Abilities []struct {
+        Ability struct {
+            Name string `json:"name"`
+            URL  string `json:"url"`
+        } `json:"ability"`
+        IsHidden bool `json:"is_hidden"`
+    } `json:"abilities"`
+ 
+    Moves []struct {
+        Move struct {
+            Name string `json:"name"`
+            URL  string `json:"url"`
+        } `json:"move"`
+    } `json:"moves"`
+ 
+    CurrentHP    int      `json:"-"`
+    StatusEffects []string `json:"-"`
+}
